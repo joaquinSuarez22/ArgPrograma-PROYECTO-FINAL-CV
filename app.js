4470d80b6ff886b3db8e3b1cfb71f8613b6cc070
Joaquin Suarez <suarezjoaco22@gmail.com>
2023-01-25 23:18:52 -0300

    preloader

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ botonColorMode.addEventListener("click", () => {
 //---------------------------------------------------------//
 
 
+var preloader = document.getElementById("preloader");
+
+window.addEventListener("load", function() {
+  preloader.style.display = "none";
+});
